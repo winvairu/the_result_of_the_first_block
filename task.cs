023e62db2c82cd123bf60003bfe55e4ba1f5d0f9
winvairu <winvairu@outlook.com>
2022-12-17 13:20:28 +0300

    add tasc.cs

---
+++ task.cs
@@ -0,0 +1,24 @@
+﻿string[] text = { "AI", "is", "the", "future", "of", "health,",
+                  "but", "safeguards", "are", "important", "too." };
+
+int len = text.Length;
+for (int i = 0; i < len; i++)
+{
+    Console.Write(text[i]);
+    Console.Write(" ");
+}
+Console.WriteLine();
+
+string[] text2 = new string[len];
+
+for (int i = 0, j = 0, size = 0; i < len; i++)
+{
+    size = text[i].Length;
+    if (size <= 3)
+    {
+        text2[j] = text[i];
+        Console.Write(text2[j]);
+        Console.Write(" ");
+        j +=1;
+    }
+}
